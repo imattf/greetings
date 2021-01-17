38545c6b926a123a71af80dabfaa541a2f931f4b
imattf <imattf@pm.me>
2021-01-17 14:07:26 -0800

    Added error handling

--- greetings.go
+++ greetings.go
@@ -1,13 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
-	var message string
-        message = fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+func Hello(name string) (string, error) {
+	// If no name given, return an error with a message.
+	if name == "" {
+		return "", errors.New("no name recieved")
+	}
+
+	// If a name is recieved, return a value that embeds the name
+	// in a greeting message.
+	message := fmt.Sprintf("Hi, %v. Welcome here!", name)
+	return message, nil
 }
-
-
